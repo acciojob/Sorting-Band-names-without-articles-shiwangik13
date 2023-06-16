d0549279d66c257998afaba23612c1437d543d49
shiwangik13 <85068938+shiwangik13@users.noreply.github.com>
2023-06-16 19:51:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort((a,b)=>{
-    let nameA = a.toLowerCase().substring(2); // ignorer upperCase
-    let nameB = b.toLowerCase().substring(2); // ignorer upperCase
-    if (nameA < nameB) {
-        return -1;
-    }
-    if (nameA > nameB) {
-    return 1;
-    }
+//arr.sort()
+//consol.log(arr)
+function strip (word) {
+    let regex = new RegExp('^(a |the |an )', 'i')
+    return word.replace(regex, '').trim()
+}
 
-    // names must be equal
-    return 0;
-})
-// Generate the list items
-const ul = document.getElementById('bands');
-touristSpots.forEach((bandName) => {
-  const li = document.createElement('li');
-  li.textContent = bandName;
-  ul.append(li);
-});
+const sortedTouristSpots  = touristSpots.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+
+document.querySelector('#bands').innerHTML = 
+    sortedTouristSpots.map(item => `<li>${item}</li>`).join('')
